ada9d9428dafd4d52cc01befb648f61bbb8c0ce5
lwwywang <liwenwang4work@163.com>
2024-02-25 21:58:54 +0800

    完成背景及主页面布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './home.scss';
+import header from '../images/header.png';
 
 export const Home = () => {
   return (
-    <div>
-      你好
+    <div className="home">
+      <header>
+        <img src={header} />
+      </header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+      </main>
     </div>
   );
 };
