e532d00cd831564f8511ee67aa9f10759d1f0e46
lwwywang <liwenwang4work@163.com>
2024-02-25 21:18:57 +0800

    init

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:FrankFang/Se1uqKnmKDZS.git &&
+git remote add origin git@github.com:lwwywang/React-BigScreen.git &&
 git push -uf origin master:gh-pages &&
 cd -;
