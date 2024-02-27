438cfdc84f3b69745d9791256da2806f3e66e049
lwwywang <liwenwang4work@163.com>
2024-02-27 21:27:58 +0800

    实时实现数据变化2

--- src/charts/Chart5.tsx
+++ src/charts/Chart5.tsx
@@ -5,11 +5,36 @@ import { createEchartsOptions } from '../shared/createEchartsOptions';
 
 const px = (n) => n / 800 * (window as any).pageWidth;
 export const Chart5 = () => {
+    const myChart = useRef(null);
     const divRef = useRef(null);
+    const data = {
+        1: [
+            { value: 0.07, name: '10-20' },
+            { value: 0.10, name: '20-30' },
+            { value: 0.23, name: '30-40' },
+            { value: 0.28, name: '40-50' },
+            { value: 0.32, name: '50-60' },
+        ],
+        2: [
+            { value: 0.09, name: '10-20' },
+            { value: 0.10, name: '20-30' },
+            { value: 0.32, name: '30-40' },
+            { value: 0.26, name: '40-50' },
+            { value: 0.23, name: '50-60' },
+        ],
+        3: [
+            { value: 0.10, name: '10-20' },
+            { value: 0.19, name: '20-30' },
+            { value: 0.25, name: '30-40' },
+            { value: 0.30, name: '40-50' },
+            { value: 0.16, name: '50-60' },
+        ]
+    };
+
+
     const colors = ['#856BED', '#F46064', '#F38E1C', '#1CDB7C', '#33A4FA'];
-    useEffect(() => {
-        const myChart = echarts.init(divRef.current);
-        myChart.setOption(createEchartsOptions({
+    const render = data => {
+        myChart.current.setOption(createEchartsOptions({
             color: ['#3597d4', '#3559a7', '#f6b044', '#ea5c5a', '#3ab059', '#fdfdfd'],
             legend: {
                 left: px(12),
@@ -40,16 +65,17 @@ export const Chart5 = () => {
                         borderColor: '#0F113A',
                         borderWidth: px(2)
                     },
-                    data: [
-                        { value: 0.07, name: '10-20' },
-                        { value: 0.10, name: '20-30' },
-                        { value: 0.23, name: '30-40' },
-                        { value: 0.28, name: '40-50' },
-                        { value: 0.32, name: '50-60' },
-                    ]
+                    data: data
                 }
             ]
         }));
+    };
+    useEffect(() => {
+        myChart.current = echarts.init(divRef.current);
+        render(data[1]);
+        setInterval(() => {
+            render(data[Math.ceil(Math.random() * 3)]);
+        }, 2000);
     }, []);
     return (
         <div className="chartWrapper">
